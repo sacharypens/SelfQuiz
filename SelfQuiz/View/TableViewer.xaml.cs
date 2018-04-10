32c2d381b5754599063c56c8309d6672609f0e67
sacharypens <sacharypens@gmail.com>
2018-04-10 13:30:36 +0200

    basis navigatie en UI

---
+++ SelfQuiz/View/TableViewer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SelfQuiz.View
+{
+    /// <summary>
+    /// Interaction logic for TableViewer.xaml
+    /// </summary>
+    public partial class TableViewer : Page
+    {
+        public TableViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
