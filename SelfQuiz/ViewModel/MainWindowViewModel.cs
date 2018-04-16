8689204bbb43992453157f24524e353cf1577feb
sacharypens <sacharypens@gmail.com>
2018-04-16 13:42:54 +0200

    aanpassingen aan navigatie

---
+++ SelfQuiz/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using BeheerContacten.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfQuiz.ViewModel
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private int selectedIndex;
+        public int SelectedIdex
+        {
+            get { return selectedIndex; }
+            set
+            {
+                if(selectedIndex == value) {
+                    return;
+                }
+                selectedIndex = value;
+                NotifyPropertyChanged();
+                
+            }
+        }
+
+        private string frameSource;
+        public string FrameSource
+        {
+            get { return frameSource; }
+            set { frameSource = value; NotifyPropertyChanged(); }
+        }
+
+        public MainWindowViewModel()
+        {
+            FrameSource = "VragenViewer.xaml";
+            selectedIndex = 0;
+        }
+
+        public void ChangePagina(int pagina)
+        {
+            switch(pagina)
+            {
+                case 0:
+                    FrameSource = "VragenViewer.xaml";
+                    break;
+                case 1:
+                    FrameSource = "SoortViewer.xaml";
+                    break;
+            }
+        }
+    }
+}
